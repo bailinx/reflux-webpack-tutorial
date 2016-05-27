32e35a9e072cd88f7f785e7340d9e913e043a2df
baili(百里) <423261989@qq.com>
2016-05-27 10:59:25 +0800

    前端工程化的一些东西

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://127.0.0.1:3000',
+        'webpack/hot/only-dev-server',
+        './fed/app.jsx'
+    ],
+    output: {
+        path: __dirname + '/public/assets',
+        publicPath: 'http://localhost:3000/assets',
+        // 直接写文件地址会有跨域等问题，还是直接url的好
+        // publicPath: '/assets',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [{
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.jsx$/,
+                loader: 'react-hot!babel-loader!jsx-loader?harmony'
+            }, {
+                test: /\.less/,
+                loader: 'style-loader!css-loader!less-loader'
+            }, {
+                test: /\.(css)$/,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }
+
+        ]
+    },
+    plugins: [
+        // 一定要加这个插件
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        // 压缩js插件
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+    ]
+}
